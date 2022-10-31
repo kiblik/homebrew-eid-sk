1aac7665a7ec385a47633aa997a5c4ad272e071e
kiblik <kiblik@gjh.sk>
2022-10-31 18:46:03 +0000

    Set versioning for eid-sk-disig-web-signer

--- Casks/eid-sk-disig-web-signer.rb
+++ Casks/eid-sk-disig-web-signer.rb
@@ -1,10 +1,15 @@
 cask 'eid-sk-disig-web-signer' do
-    version :latest
-    sha256 :no_check
+    version "2.0.7-1"
+    sha256 "da4229962fbf0c17c216516041e29991576ebd5d6f2d280d1e6bc01462bf727d"
 
-    url 'https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer.dmg'
+    url 'https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer_#{version}.dmg'
     name 'Aplikacia pre ZEP a KEP'
     homepage 'https://www.slovensko.sk/sk/titulna-stranka'
+
+    livecheck do
+        url "https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer.dmg"
+        regex(/Disig_Web_Signer_(\d+(?:[\.\-]\d+)+).dmg/i)
+    end
 
     pkg 'Disig_Web_Signer.pkg'
 
