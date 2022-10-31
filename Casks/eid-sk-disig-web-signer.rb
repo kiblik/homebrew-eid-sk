3de54d34f5755803475f7c7f9cd266fa7a59f4e6
kiblik <kiblik@gjh.sk>
2022-10-31 18:53:29 +0000

    Fix url

--- Casks/eid-sk-disig-web-signer.rb
+++ Casks/eid-sk-disig-web-signer.rb
@@ -2,7 +2,7 @@ cask 'eid-sk-disig-web-signer' do
     version "2.0.7-1"
     sha256 "da4229962fbf0c17c216516041e29991576ebd5d6f2d280d1e6bc01462bf727d"
 
-    url 'https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer_#{version}.dmg'
+    url "https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer_#{version}.dmg"
     name 'Aplikacia pre ZEP a KEP'
     homepage 'https://www.slovensko.sk/sk/titulna-stranka'
 
