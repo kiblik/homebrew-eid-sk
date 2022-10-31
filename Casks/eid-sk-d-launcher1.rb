f98cc27a5dc640ed1695c256db173f156412a330
kiblik <kiblik@gjh.sk>
2022-10-31 18:18:16 +0000

    add eid-sk-d-launcher1

---
+++ Casks/eid-sk-d-launcher1.rb
@@ -0,0 +1,11 @@
+cask 'eid-sk-d-launcher1' do
+    version :latest
+    sha256 :no_check
+
+    # eidas.minv.sk/TCTokenService/download was verified as official when first introduced to the cask
+    url 'https://www.slovensko.sk/static/zep/apps/DLauncher.mac.dmg'
+    name 'Aplikacia pre ZEP a KEP'
+    homepage 'https://www.slovensko.sk/sk/titulna-stranka'
+
+    app 'D.Launcher'
+end
