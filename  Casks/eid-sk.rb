4e49c53df4c8a83eeffb298bde2e868a471ad337
kiblik <kiblik@gjh.sk>
2022-10-31 17:44:24 +0000

    Fix quotes

---  Casks/eid-sk.rb
+++  Casks/eid-sk.rb
@@ -1,6 +1,6 @@
 cask 'eid-sk' do
-    version 4.1
-    sha256 3206e00c6de5c0cc10b35ecc10d565861ee08ece8347460e593cb9882dbd22bd
+    version '4.1'
+    sha256 '3206e00c6de5c0cc10b35ecc10d565861ee08ece8347460e593cb9882dbd22bd'
 
     # eidas.minv.sk/TCTokenService/download was verified as official when first introduced to the cask
     url 'https://eidas.minv.sk/downloadservice/eidklient/mac/eID_klient.dmg'
