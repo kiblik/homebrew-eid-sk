211c9fbb203f5ed36754631ad80bae9955cc82cb
Tomas Kubla <tomas@kubla.sk>
2022-12-05 15:43:41 +0000

    Consolidation - fix checks

--- Casks/eid-sk-d-launcher1.rb
+++ Casks/eid-sk-d-launcher1.rb
@@ -1,11 +1,12 @@
-cask 'eid-sk-d-launcher1' do
-    version :latest
-    sha256 :no_check
+cask "eid-sk-d-launcher1" do
+  version :latest
+  sha256 :no_check
 
-    # eidas.minv.sk/TCTokenService/download was verified as official when first introduced to the cask
-    url 'https://www.slovensko.sk/static/zep/apps/DLauncher.mac.dmg'
-    name 'Aplikacia pre ZEP a KEP'
-    homepage 'https://www.slovensko.sk/sk/titulna-stranka'
+  # eidas.minv.sk/TCTokenService/download was verified as official when first introduced to the cask
+  url "https://www.slovensko.sk/static/zep/apps/DLauncher.mac.dmg"
+  name "Aplikacia pre ZEP a KEP"
+  desc "Application for Qualified Electronic Signature"
+  homepage "https://www.slovensko.sk/sk/titulna-stranka"
 
-    app 'D.Launcher'
+  app "D.Launcher"
 end
--- Casks/eid-sk.rb
+++ Casks/eid-sk.rb
@@ -1,19 +1,21 @@
-cask 'eid-sk' do
-    version '4.3'
-    sha256 '776341f1d929218636574ff9bc7f133552981e85c0a20772f1a486e40e96c461'
+cask "eid-sk" do
+  version "4.3"
+  sha256 :no_check
 
-    # eidas.minv.sk/TCTokenService/download was verified as official when first introduced to the cask
-    url 'https://eidas.minv.sk/downloadservice/eidklient/mac/eID_klient.dmg'
-    name 'Aplikacia pre eID'
-    name 'eID Klient'
-    name 'Electronic identity card software for Slovakia'
-    name 'eID Slovakia'
-    homepage 'https://www.slovensko.sk/sk/titulna-stranka'
+  # eidas.minv.sk/TCTokenService/download was verified as official when first introduced to the cask
+  url "https://eidas.minv.sk/downloadservice/eidklient/mac/eID_klient.dmg",
+      verified: "eidas.minv.sk/downloadservice/eidklient/mac/"
+  name "Aplikacia pre eID"
+  name "eID Klient"
+  name "Electronic identity card software for Slovakia"
+  name "eID Slovakia"
+  desc "Application for log in by Slovak eID"
+  homepage "https://www.slovensko.sk/sk/titulna-stranka"
 
-    livecheck do
-        url "https://eidas.minv.sk/download"
-        regex(/eID klient (\d+(?:\.\d+)+) pre macOS/i)
-    end
+  livecheck do
+    url "https://eidas.minv.sk/download"
+    regex(/eID klient (\d+(?:\.\d+)+) pre macOS/i)
+  end
 
-    app 'eID_klient.app'
+  app "eID_klient.app"
 end
