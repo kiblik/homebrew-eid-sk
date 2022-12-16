c4a2d08366c49ee510b1584575d469743ae02973
kiblik <kiblik@gjh.sk>
2022-12-16 16:34:21 +0000

    Fix some syntax

--- Casks/eid-sk-disig-web-signer.rb
+++ Casks/eid-sk-disig-web-signer.rb
@@ -2,14 +2,15 @@ cask "eid-sk-disig-web-signer" do
   version "2.0.7-1"
   sha256 "da4229962fbf0c17c216516041e29991576ebd5d6f2d280d1e6bc01462bf727d"
 
-  url "https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer_#{version}.dmg"
+  url "https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer_#{version}.dmg",
+      verified: "download.disigcdn.sk/cdn/products/websigner2/"
   name "Aplikacia pre ZEP a KEP"
   desc "Application for Qualified Electronic Signature"
   homepage "https://www.slovensko.sk/sk/titulna-stranka"
 
   livecheck do
     url "https://download.disigcdn.sk/cdn/products/websigner2/Disig_Web_Signer.dmg"
-    regex(/Disig_Web_Signer_(\d+(?:[.\-]\d+)+).dmg/i)
+    regex(/Disig_Web_Signer_(\d+(?:[.-]\d+)+).dmg/i)
   end
 
   pkg "Disig_Web_Signer.pkg"
