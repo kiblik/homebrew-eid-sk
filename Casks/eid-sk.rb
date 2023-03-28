3fe7de1b0ee3d4bbb6546c21d2bd5b2c54230578
kiblik <kiblik@gjh.sk>
2023-03-28 12:30:20 +0200

    Upgrade eid-sk to 4.6

--- Casks/eid-sk.rb
+++ Casks/eid-sk.rb
@@ -1,6 +1,6 @@
 cask "eid-sk" do
-  version "4.5"
-  sha256 "fe37fc8317f1a8b5b8b10e368b6b372f545a6959210ed1ff0d552f8129937f2e"
+  version "4.6"
+  sha256 "c77b23723dab687973a9d469d02af6620f074897af6f7b433cdf45b5040af840"
 
   url "https://eidas.minv.sk/downloadservice/eidklient/mac/eID_klient.dmg#v=#{version}",
       verified: "eidas.minv.sk/downloadservice/eidklient/mac/"
